091ad55541270cc7a3f5652fac0274322e5bb898
Arden Ranta <arranta20@gmail.com>
2023-08-01 19:17:19 -0700

    Add places array.

---
+++ friends.js
@@ -0,0 +1,8 @@
+// var arrays = ["Lam", "Sam", "Lex", "Joe", "Liz"];
+// console.log(arrays[2]);
+// console.log(arrays[4]);
+// console.log(arrays[0]);
+
+var places = ["France", "China", "Thailand", "Japan", "Ireland"];
+places.pop();
+console.log(places);
